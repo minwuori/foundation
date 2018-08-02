f6d0b80ca64cf10e62766471a611afe4bdd5fd31
minwuori dikova <minwuori@mail.ru>
2018-08-02 10:47:16 +0300

    start

---
+++ src/helpers/bg_white.js
@@ -0,0 +1,5 @@
+module.exports = function(options) {
+  // options.fn(this) = Handelbars content between {{#bold}} HERE {{/bold}}
+  var bolder = '<strong>' + options.fn(this) + '</strong>';
+  return bolder;
+}
